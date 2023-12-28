0d58e3d9309d29962e664623c4ed1752b2998387
Raúl Pera Pairó <raul_pairo@icloud.com>
2023-12-28 12:44:08 +0100

    Initial Commit

---
+++ Ecommerce/EcommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceApp.swift
+//  Ecommerce
+//
+//  Created by Raúl Pera Pairó on 28/12/23.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
