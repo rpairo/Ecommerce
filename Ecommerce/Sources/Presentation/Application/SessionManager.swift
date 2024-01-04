3870f2ac83115c4a3a5c9906af37ca74ff2ffe06
Raúl Pera Pairó <raul_pairo@icloud.com>
2024-01-04 15:25:06 +0100

    feat: add session manager

---
+++ Ecommerce/Sources/Presentation/Application/SessionManager.swift
@@ -0,0 +1,12 @@
+//
+//  SessionManager.swift
+//  Ecommerce
+//
+//  Created by Raúl Pera Pairó on 30/12/23.
+//
+
+import Foundation
+
+class SessionManager: ObservableObject {
+    @Published var isAuthenticated: Bool = false
+}
